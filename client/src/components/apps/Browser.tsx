af8b13b32eb66dbefa18569b5ae66f63ace127c3
Nova Arctic <nova@zeusteam.dev>
2025-04-09 16:49:50 +1000

    Pull from 617497008bef906e7e7b32ab6aa0df7ee0278353

--- client/src/components/apps/Browser.tsx
+++ client/src/components/apps/Browser.tsx
@@ -153,7 +153,7 @@ export function Browser() {
               )}
             >
               <iframe
-                ref={el => iframeRefs.current[tab.id] = el}
+                ref={el => { iframeRefs.current[tab.id] = el }}
                 src={tab.url}
                 className="w-full h-full"
                 sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals"
