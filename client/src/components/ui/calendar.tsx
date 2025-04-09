617497008bef906e7e7b32ab6aa0df7ee0278353
Nova Arctic <nova@zeusteam.dev>
2025-04-09 14:18:32 +1000

    massive: Fix everything broken by package updates

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -52,9 +52,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
+      } as any}
       {...props}
     />
   )
