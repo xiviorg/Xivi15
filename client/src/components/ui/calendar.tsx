1f159c6f7abc14a84486e61d46f466355a5b8500
Nova Arctic <nova@zeusteam.dev>
2025-04-09 16:18:53 +1000

    Revert update, we'll cherry-pick these commits later

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -52,9 +52,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
-      } as any}
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+      }}
       {...props}
     />
   )
